ac9a143dff3f22f71c5f12470a776166f65bc04f
rodrigopetito <rodrigo.petito@tictapps.com>
2015-07-03 09:54:18 -0300

    algo

---
+++ App/app/src/main/java/com/example/tictapps/app/Utils/ScrollState.java
@@ -0,0 +1,22 @@
+package com.example.tictapps.app.Utils;
+
+/**
+ * Created by tictapps on 30/06/15.
+ */
+public enum ScrollState {
+    /**
+     * Widget is stopped.
+     * This state does not always mean that this widget have never been scrolled.
+     */
+    STOP,
+
+    /**
+     * Widget is scrolled up by swiping it down.
+     */
+    UP,
+
+    /**
+     * Widget is scrolled down by swiping it up.
+     */
+    DOWN,
+}
---
+++ App/app/src/main/java/com/example/tictapps/app/Utils/Scrollable.java
@@ -0,0 +1,45 @@
+package com.example.tictapps.app.Utils;
+
+/**
+ * Created by tictapps on 30/06/15.
+ */
+import android.view.ViewGroup;
+
+import com.example.tictapps.app.Utils.ObservableScrollViewCallbacks;
+
+/**
+ * Provides common API for observable and scrollable widgets.
+ */
+public interface Scrollable {
+    /**
+     * Sets a callback listener.
+     *
+     * @param listener listener to set
+     */
+    void setScrollViewCallbacks(ObservableScrollViewCallbacks listener);
+
+    /**
+     * Scrolls vertically to the absolute Y.
+     * Implemented classes are expected to scroll to the exact Y pixels from the top,
+     * but it depends on the type of the widget.
+     *
+     * @param y vertical position to scroll to
+     */
+    void scrollVerticallyTo(int y);
+
+    /**
+     * Returns the current Y of the scrollable view.
+     *
+     * @return current Y pixel
+     */
+    int getCurrentScrollY();
+
+    /**
+     * Sets a touch motion event delegation ViewGroup.
+     * This is used to pass motion events back to parent view.
+     * It's up to the implementation classes whether or not it works.
+     *
+     * @param viewGroup ViewGroup object to dispatch motion events
+     */
+    void setTouchInterceptionViewGroup(ViewGroup viewGroup);
+}
